aa3a51c33068767a89d7c25b2acdb866837ae97c
Maya <fox>
2023-07-07 11:34:44 +0200

    Añadir acceso

--- webinterface.py
+++ webinterface.py
@@ -5,7 +5,6 @@ from database import almacenar
 from database import buscar
 
 
-
 app = Flask(__name__, template_folder="templates")
 
 
@@ -13,16 +12,18 @@ app = Flask(__name__, template_folder="templates")
 def index():
     return render_template("index.html")
 
+
 @app.route("/escuchar_url_larga", methods=["POST"])
 def action():
     url_larga = request.json["url_larga"]
-    codigo = generar_url_corta(url_larga)   
+    codigo = generar_url_corta(url_larga)
     almacenar(codigo, url_larga)
     url_corta = "http://equipo.a/" + codigo
 
     return {"respuesta": url_corta}
 
-@app.route('/<string:codigo>', methods=['GET'])
+
+@app.route("/<string:codigo>", methods=["GET"])
 def redirigir(codigo):
     print(codigo)
     return redirect("http://www.example.com", code=302)
@@ -30,4 +31,4 @@ def redirigir(codigo):
 
 # run the application
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=8000, debug=True, host="0.0.0.0")
