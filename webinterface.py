65afe7d4103d0c36c67e0c50a4116be9cc58d7bf
LAPTOP-185UEBDF\amoya <Andres>
2023-07-07 12:54:00 +0200

    agregamos portweb y en webinterrface

--- webinterface.py
+++ webinterface.py
@@ -1,9 +1,19 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, url_for, flash, redirect
 from random import randint
 from acortador import generar_url_corta
 from database import almacenar
 from database import buscar
 
+load_dotenv()
+
+config = {
+    'host': os.getenv('SERVER'),
+    'portweb': os.getenv('PORTWEB')
+
+}
+ 
 
 app = Flask(__name__, template_folder="templates")
 
@@ -23,7 +33,9 @@ def action():
     url_larga = request.json["url_larga"]
     codigo = generar_url_corta(url_larga)
     almacenar(codigo, url_larga)
-    url_corta = "http://equipo.a/" + codigo
+    url_corta = f'{config["host"]}:{config["portweb"]}/{codigo}'
+
+    # url_corta = "http://equipo.a/" + codigo
 
     return {"respuesta": url_corta}
 
